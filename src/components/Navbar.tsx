062b226785161fef58aaf5a758e04095195fe1d3
huangfishburger <sophia.ythuang@gmail.com>
2023-12-28 04:09:17 +0800

    feat: update logo size and navigation styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,6 +13,8 @@ export function Navbar() {
   const [open, setOpen] = React.useState(false);
   const [isScrolling, setIsScrolling] = React.useState(false);
 
+  const navBarStyle = "text-2xl p-4 hover:font-bold hover:text-dark-blue hover:border-b-4";
+
   // Effect for handling resize events
   React.useEffect(() => {
     const handleResize = () => window.innerWidth >= 960 && setOpen(false);
@@ -37,26 +39,23 @@ export function Navbar() {
       shadow={false}
       blurred={false}
       color={isScrolling ? "white" : "transparent"}
-      className="fixed top-0 z-50 border-0"
+      className="fixed top-0 z-50 border-0 text-black backdrop-blur-xl bg-opacity-80"
       placeholder={open ? "Loading..." : undefined}
     >
       <div className="container mx-auto flex items-center justify-between">
-        <Image src="/logo.png" alt="GoGoFund Logo" width={400} height={250} />
+        <Image src="/logo.png" alt="GoGoFund Logo" width={300} height={200} />
         <ul
-          className={`ml-10 hidden items-center gap-6 lg:flex ${
-            isScrolling ? "text-gray-900" : "text-black"
-          }`}
+          className={`ml-10 hidden items-center gap-6 lg:flex ${isScrolling ? "text-gray-900" : "text-black"
+            }`}
         >
-          <Link href="/events">All Events</Link>
-          <Link href="/history">Our Team</Link>
-          <Link href="/collection">My Collection</Link>
+          <Link href="/events" className={navBarStyle}>All Events</Link>
+          <Link href="/history" className={navBarStyle}>Our Team</Link>
+          <Link href="/collection" className={navBarStyle}>My Collection</Link>
         </ul>
         <div className="flex flex-row space-x-8">
-          <Button variant="contained" className="h-10 text-xl text-black">
+          <button className="h-10 text-xl text-white bg-dark-blue p-4 flex items-center justify-center rounded-2xl font-bold">
             Get Fund
-          </Button>
-        </div>
-        <div className="hidden gap-2 lg:flex lg:items-center">
+          </button>
           <ConnectButton />
         </div>
       </div>
