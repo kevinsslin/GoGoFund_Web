8fdb9df1f8b5889bccad8daae61bbb426c83e59b
huangfishburger <sophia.ythuang@gmail.com>
2023-12-27 21:37:16 +0800

    feat:eventcard

---
+++ src/app/events/_components/EventCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineFieldTime } from "react-icons/ai";
+
+import Image from "next/image";
+
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+export type CardListProps = {
+  name: string;
+  progess: number;
+  money: number;
+  person: number;
+  time: number;
+};
+
+export default function EventCard({
+  name,
+  progess,
+  money,
+  person,
+  time,
+}: CardListProps) {
+  const handleClick = () => {
+    console.log("click");
+  };
+
+  const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    width: "100%",
+    borderRadius: 5,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
+    },
+  }));
+
+  return (
+    <>
+      <Paper className="w-50 p-2 hover:cursor-pointer" onClick={handleClick}>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/events.jpeg"
+            alt="event"
+            width={300}
+            height={300}
+            className="p-5 "
+          />
+          <Typography className="break-all font-bold" variant="h4">
+            {name}
+          </Typography>
+        </div>
+        <p className="pl-6 text-xl">NT$ {money}</p>
+        <div className="flex flex-row items-center p-2">
+          <BorderLinearProgress
+            variant="determinate"
+            className="m-2"
+            value={progess}
+          />
+          <p>{progess}%</p>
+        </div>
+        <div className="flex flex-row items-center justify-end pb-2">
+          <AiOutlineUser />
+          <p className="p-2 font-bold">{person}</p>
+          <AiOutlineFieldTime />
+          <p className="p-2 font-bold">{time}</p>
+        </div>
+      </Paper>
+    </>
+  );
+}
