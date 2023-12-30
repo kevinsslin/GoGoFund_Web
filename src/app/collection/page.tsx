e6c02a359eb1eff09906a1b2bd953d65acf8d91e
kevinsslin <kk123750964@gmail.com>
2023-12-30 16:03:14 +0800

    feat: add dbEvents state and fetch events from API

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -9,6 +9,7 @@ import { useAccount } from "wagmi";
 import UserDialog from "@/components/UserDialog";
 
 import AvatarSelector from "./_component/AvatarSelect";
+import type { allEventDto } from "@/lib/types/db";
 
 function CollectionPage() {
   const [open, setOpen] = useState(false);
@@ -16,57 +17,27 @@ function CollectionPage() {
   const [email, setEmail] = useState("");
   const { address } = useAccount();
 
+  const [dbEvents, setDbEvents] = useState<allEventDto[]>([]);
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchUser = async () => {
       const response = await fetch(`/api/users/${address}`);
       const data = await response.json();
       setName(data.username);
       setEmail(data.email);
     };
-    fetchData();
+    const fetchEvents = async () => {
+      const reponse = await fetch(`/api/mycollection/${address}`);
+      const data = await reponse.json();
+      setDbEvents(data);
+    }
+    fetchUser();
+    fetchEvents();
   }, [address]);
 
   const handelClick = () => {
     setOpen(true);
   };
-
-  const mockdbevent = [
-    {
-      name: "name",
-      progess: 92.77,
-      person: 1000,
-      money: 9999,
-      time: 20,
-      id: 1,
-    },
-    { name: "apple", progess: 9.7, person: 102, money: 9, time: 320, id: 2 },
-    { name: "kevin", progess: 10.1, person: 1, money: 1314, time: 200, id: 3 },
-    {
-      name: "wp1121",
-      progess: 22.1,
-      person: 30,
-      money: 99678,
-      time: 48,
-      id: 4,
-    },
-    { name: "ntu", progess: 59.77, person: 92, money: 778594, time: 60, id: 5 },
-    {
-      name: "kk",
-      progess: 80.2,
-      person: 1980,
-      money: 95532,
-      time: 3944,
-      id: 6,
-    },
-    {
-      name: "books",
-      progess: 100.77,
-      person: 863,
-      money: 78443,
-      time: 302,
-      id: 7,
-    },
-  ];
 
   return (
     <main className="flex flex-row justify-center space-x-40 pl-32 pr-32">
@@ -80,7 +51,7 @@ function CollectionPage() {
         >
           edit profile
         </button>
-        <p className="pt-6 text-lg">{mockdbevent.length} collections</p>
+        <p className="pt-6 text-lg">{dbEvents.length} collections</p>
       </div>
       <div className="flex w-[60%] flex-col items-start">
         <p className="flex justify-start p-2 text-4xl font-bold">
@@ -93,10 +64,10 @@ function CollectionPage() {
         />
         <div className="p-4"></div>
         <Grid container spacing={3} direction="row" justifyContent="flex-start">
-          {mockdbevent.length === 0 ? (
+          {dbEvents.length === 0 ? (
             <p className="text-4xl">You have no events now.</p>
           ) : (
-            mockdbevent.map((e) => (
+            dbEvents.map((e) => (
               <Grid
                 item
                 xs={12}
