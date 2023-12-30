60a6c517b37f0049dacb6c0d661a9d66f36d9886
huangfishburger <sophia.ythuang@gmail.com>
2023-12-30 18:11:34 +0800

    feat:update circle progress

--- src/app/events/[eventId]/page.tsx
+++ src/app/events/[eventId]/page.tsx
@@ -19,7 +19,7 @@ import type { eventDetailDto } from "@/lib/types/db";
 import FundDialog from "./_components/FundDialog";
 
 function CircularProgressWithLabel(
-  props: CircularProgressProps & { value: number },
+  props: CircularProgressProps & { value: number }, truevalue: number
 ) {
   return (
     <Box sx={{ position: "relative", display: "inline-flex" }}>
@@ -89,7 +89,7 @@ function EventsIdPage() {
           <p className="p-4 text-6xl font-bold">{dbEvent?.title}</p>
           <div className="flex flex-row items-center p-4">
             <CircularProgressWithLabel
-              value={(dbEvent.currentValue / dbEvent.targetValue) * 100}
+              value={(dbEvent.currentValue / dbEvent.targetValue) * 100 > 100 ? 100 : (dbEvent.currentValue / dbEvent.targetValue) * 100}
             />
             <div className="pl-8">
               <p className="text-md pb-2">{`Target Amount: NTD$ ${dbEvent?.targetValue}`}</p>
